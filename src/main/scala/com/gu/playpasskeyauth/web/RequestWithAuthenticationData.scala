e981c28a61b719a9cd5a91d9c99e78571700253a
Kelvin Chappell <1722550+kelvin-chappell@users.noreply.github.com>
2025-12-14 21:26:48 +0000

    docs: Add scaladoc to RequestWithAuthenticationData

--- src/main/scala/com/gu/playpasskeyauth/web/RequestWithAuthenticationData.scala
+++ src/main/scala/com/gu/playpasskeyauth/web/RequestWithAuthenticationData.scala
@@ -6,20 +6,80 @@ import play.api.mvc.Results.BadRequest
 
 import scala.concurrent.{ExecutionContext, Future}
 
+/** A request wrapper that carries passkey authentication data along with the user and original request.
+  *
+  * This is used during passkey authentication to pass the credential assertion response from the browser's WebAuthn API
+  * through the request processing chain.
+  *
+  * @tparam U
+  *   The user type
+ *
+  * @tparam A
+  *   The body content type (e.g., `AnyContent`, `JsValue`)
+  *
+  * @param authenticationData
+  *   The JSON response from `navigator.credentials.get()` in the browser. This contains the signed assertion data
+  *   needed to verify the passkey.
+ *
+  * @param user
+  *   The user attempting to authenticate
+ *
+  * @param request
+  *   The original Play request being wrapped
+  */
 class RequestWithAuthenticationData[U, A](
     val authenticationData: JsValue,
     val user: U,
     request: Request[A]
 ) extends WrappedRequest[A](request)
 
-/** There are any number of ways to find authentication data in a request so this is left to the implementer of this
-  * trait to decide. This authentication data is the json that's returned by a `navigator.credentials.get` call in a
-  * browser. It's optional in case the request doesn't actually contain the data.
+/** Extractor for WebAuthn authentication data from a request.
+  *
+  * Implementations define how to find the JSON data returned by `navigator.credentials.get()` in the browser. This data
+  * is typically sent in the request body or as a header.
+  *
+  * @tparam R
+  *   The request type (must be a subtype of Play's `Request`)
+  *
+  * @example
+  *   {{{
+  * // Extract authentication data from a JSON request body
+  * given AuthenticationDataExtractor[[A] =>> RequestWithUser[MyUser, A]] with {
+  *   def findAuthenticationData[A](request: RequestWithUser[MyUser, A]): Option[JsValue] = {
+  *     request.body match {
+  *       case body: AnyContent => body.asJson.flatMap(json => (json \ "assertion").toOption)
+  *       case _ => None
+  *     }
+  *   }
+  * }
+  *
+  * // Or extract from a custom header (if sending as base64-encoded JSON)
+  * given AuthenticationDataExtractor[[A] =>> RequestWithUser[MyUser, A]] with {
+  *   def findAuthenticationData[A](request: RequestWithUser[MyUser, A]): Option[JsValue] = {
+  *     request.headers.get("X-Passkey-Assertion")
+  *       .map(base64 => Json.parse(Base64.getDecoder.decode(base64)))
+  *   }
+  * }
+  *   }}}
   */
 trait AuthenticationDataExtractor[R[_] <: Request[_]] {
   def findAuthenticationData[A](request: R[A]): Option[JsValue]
 }
 
+/** Action refiner that extracts passkey authentication data from a [[RequestWithUser]].
+  *
+  * This action refiner is used during passkey authentication to extract the credential assertion response from the
+  * browser. If the authentication data is missing, a `BadRequest` response is returned.
+  *
+  * @tparam U
+  *   The user type
+  *
+  * @param authDataExtractor
+  *   Strategy for extracting the WebAuthn assertion response JSON
+ *
+  * @param executionContext
+  *   The execution context for async operations
+  */
 class AuthenticationDataAction[U](
     authDataExtractor: AuthenticationDataExtractor[[A] =>> RequestWithUser[U, A]]
 )(using val executionContext: ExecutionContext)
